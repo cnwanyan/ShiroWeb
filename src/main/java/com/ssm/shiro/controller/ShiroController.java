48dca4c39bc75ac8e2b2a405f507ddd84076dea2
wyxccs <wyxccs@163.com>
2017-03-16 08:45:28 +0800

    Update Controller

--- src/main/java/com/ssm/shiro/controller/ShiroController.java
+++ src/main/java/com/ssm/shiro/controller/ShiroController.java
@@ -8,6 +8,7 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 @RequestMapping("/shiro")
 public class ShiroController {
