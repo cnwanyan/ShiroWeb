52b9e5d5d55b90c1d510aaa77e5578051f541e5a
wyxccs <wyxccs@163.com>
2017-03-16 15:13:17 +0800

    Add annotation

--- src/main/java/com/ssm/shiro/factory/FilterChainDefinitionMapFactory.java
+++ src/main/java/com/ssm/shiro/factory/FilterChainDefinitionMapFactory.java
@@ -2,8 +2,18 @@ package com.ssm.shiro.factory;
 
 import java.util.LinkedHashMap;
 
+/**
+ * 用来创建权限列表的工厂类
+ * @author cs
+ *
+ */
 public class FilterChainDefinitionMapFactory {
 	
+	
+	/**
+	 * 可以使用数据库调取权限
+	 * @return
+	 */
 	public LinkedHashMap<String,String> getFilterChainDefinitionMap(){
 		LinkedHashMap<String,String> map=new LinkedHashMap<String,String>();
 		map.put("/login.jsp", "anon") ;
